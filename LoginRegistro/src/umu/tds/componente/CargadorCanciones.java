268465d800cbac254557b1585a13ad5416420070
JJ <jjmendezlagunas@um.es>
2024-06-17 02:13:50 +0200

    2º Commit

---
+++ LoginRegistro/src/umu/tds/componente/CargadorCanciones.java
@@ -0,0 +1,38 @@
+package umu.tds.componente;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+
+public class CargadorCanciones implements Serializable{
+
+	private static CargadorCanciones instancia;
+	private String archivoCanciones;
+	private PropertyChangeSupport cancionesNuevas;
+	
+	public CargadorCanciones() 
+	{
+		cancionesNuevas = new PropertyChangeSupport(this);
+	}
+	
+	public static CargadorCanciones getUnicaInstancia() {
+		if (instancia == null) {
+			instancia = new CargadorCanciones();
+		}
+		return instancia;
+	}
+	
+	public void setSueldo(String canciones) {
+		String oldCanciones = this.archivoCanciones;
+		this.archivoCanciones = canciones;
+		cancionesNuevas.firePropertyChange("archivoCanciones", oldCanciones, archivoCanciones);
+	}
+	
+	public void addSueldoChangeListener(PropertyChangeListener pcl) {
+		cancionesNuevas.addPropertyChangeListener(pcl);
+	}
+	
+	public void removeSueldoChangeListener(PropertyChangeListener pcl) {
+		cancionesNuevas.removePropertyChangeListener(pcl);
+	} 
+}
